0c311781799146675d9fed13bf86085361270c09
Akito Mizuno <mznakt@gmail.com>
2018-02-18 22:34:22 +0900

    making start todo-app

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 (function() {
   'use strict';
+
+  let vm = new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3',
+      ]
+    }
+  });
 })();
